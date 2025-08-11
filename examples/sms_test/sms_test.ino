65f9ce2d07aa584633c5189a586a3a141f88546f
DAO NGUYEN <54179901+daonguyen207@users.noreply.github.com>
2025-08-11 19:16:51 +0700

    Update sms_test.ino

--- examples/sms_test/sms_test.ino
+++ examples/sms_test/sms_test.ino
@@ -1,11 +1,14 @@
 #define SIM_SERIAL         Serial2  //khai báo serial cho module sim
 #define SIM_ENABLE_LOG              //bật cái này nếu muốn in log thư viện
-//#define SIM_LOG_RX_UART           //bật cái này nếu muốn in toàn bộ data nhận được từ uart
+#define SIM_LOG_RX_UART           //bật cái này nếu muốn in toàn bộ data nhận được từ uart
 
 #include "sim76xx_api.h"
 #include "sms_call_at.h"
 
-void SIM_on_sms(String phone, String sms)
+#define TX_SIM1 19
+#define RX_SIM1 21
+
+void SIM_on_sms(String phone, String sms, String time)
 {
   Serial.println("=>SMS RX: " + phone + "," + sms);
 }
@@ -24,14 +27,23 @@ void setup()
 {
   Serial.begin(115200);
   SIM_SERIAL.setRxBufferSize(2048);
-  SIM_SERIAL.begin(115200);
+  //SIM_SERIAL.begin(115200);
+  SIM_SERIAL.begin(115200,SERIAL_8N1,TX_SIM1,RX_SIM1);
   SIM7600_on_ready_callback(SIM_on_ready);
+  SIM7600_noLTE(true); // tắt mạng
 }
 
 void loop() 
 {
   SIM7600_loop();
-  
-  //sms_sent("0398647714","Xin chao, toi la sms gui tu esp");
+  if(Serial.available()) //nếu gõ a thì gửi sms
+  {
+    if(Serial.read() == 'a')
+    {
+      int result = sms_sent_andWait("0394733311","Xin chào các bạn, tôi là IOT47",5000);
+      if(result == -1)Serial.println("Gửi sms thất bại");
+      else Serial.println("Gửi sms thành công");
+    }
+  }
   //call_calling("0398647714");
 }
